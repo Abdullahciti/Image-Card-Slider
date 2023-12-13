4e8e55becd3109726e0bea0b4c579a054820befd
Top C <136592513+Abdullahciti@users.noreply.github.com>
2023-12-13 15:54:58 +0100

    Add files via upload

--- notMain.js
+++ notMain.js
@@ -1,6 +1,6 @@
 let wrapper = document.querySelector(".wrapper"),
 imagesContainer = document.querySelector(".images-container"),
-buttons = document.querySelectorAll("i"),
+buttons = document.querySelectorAll(".button"),
 imgs = Array.from(document.querySelectorAll("img"))
 bulletsContainer = document.querySelector(".bullets ul"),
 carousel = document.querySelector(".carousel");
